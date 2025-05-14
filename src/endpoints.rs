e9669abbca61ea8575ecbedb73adad80c5c62e2c
Jonas Arter <jonasarter@outlook.com>
2025-05-14 21:08:56 +0200

    added compile endpoint; not done yet

--- src/endpoints.rs
+++ src/endpoints.rs
@@ -1,3 +1,4 @@
+mod compile;
 mod execute;
 
 pub use execute::{ExecuteRequest, ExecuteResponse};
--- src/endpoints/compile.rs
+++ src/endpoints/compile.rs
@@ -3,9 +3,17 @@ use serde::{Deserialize, Serialize};
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, Serialize)]
-pub struct ExecuteRequest {
+pub struct CompileRequest {
+    pub target: String,
+    #[serde(rename = "assemblyFlavor")]
+    pub assembly_flavor: Option<String>,
+    #[serde(rename = "demangleAssembly")]
+    pub demangle_assembly: Option<String>,
+    #[serde(rename = "processAssembly")]
+    pub process_assembly: Option<String>,
     pub channel: Channel,
     pub mode: Mode,
+    #[serde(default)]
     pub edition: Edition,
     #[serde(rename = "crateType")]
     pub crate_type: CrateType,
@@ -15,35 +23,13 @@ pub struct ExecuteRequest {
     pub code: String,
 }
 
-impl ExecuteRequest {
-    #[allow(dead_code)]
-    fn new(
-        channel: Channel,
-        mode: Mode,
-        edition: Edition,
-        crate_type: CrateType,
-        tests: bool,
-        backtrace: bool,
-        code: String,
-    ) -> ExecuteRequest {
-        ExecuteRequest {
-            channel,
-            mode,
-            edition,
-            crate_type,
-            tests,
-            backtrace,
-            code,
-        }
-    }
-}
-
 #[allow(dead_code)]
 #[derive(Debug, Clone, Deserialize)]
-pub struct ExecuteResponse {
+pub struct CompileResponse {
     pub success: bool,
     #[serde(rename = "exitDetail")]
     pub exit_detail: String,
+    pub code: String,
     pub stdout: String,
     pub stderr: String,
 }
